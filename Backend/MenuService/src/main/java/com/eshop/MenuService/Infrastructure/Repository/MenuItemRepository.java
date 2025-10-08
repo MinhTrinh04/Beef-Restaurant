426f1cc830b23a30294d7f183dfa502cf4afe158
MinhTrinh04 <minhfananime@gmail.com>
2025-10-08 21:54:07 +0700

    update Repository

---
+++ Backend/MenuService/src/main/java/com/eshop/MenuService/Infrastructure/Repository/MenuItemRepository.java
@@ -0,0 +1,15 @@
+package com.eshop.MenuService.Infrastructure.Repository;
+
+
+import com.eshop.MenuService.Model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
+
+    //Select * from menu_items where menu_category_id = ?
+    Optional<MenuItem> findByMenuCategoryId(int categoryid);
+}
