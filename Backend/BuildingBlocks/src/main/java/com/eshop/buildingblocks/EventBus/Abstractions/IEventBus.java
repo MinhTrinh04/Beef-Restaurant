c1144b1b003a33b6359eb4018d7b058279182a8d
MinhTrinh04 <minhfananime@gmail.com>
2025-10-13 16:30:33 +0700

    Fix buildingblocks

--- Backend/BuildingBlocks/src/main/java/com/eshop/buildingblocks/EventBus/Abstractions/IEventBus.java
+++ Backend/BuildingBlocks/src/main/java/com/eshop/buildingblocks/EventBus/Abstractions/IEventBus.java
@@ -4,4 +4,6 @@ import com.eshop.buildingblocks.EventBus.Events.IntegrationEvent;
 
 public interface IEventBus {
     void publish(IntegrationEvent event);
+    <T extends IntegrationEvent> void subscribe(Class<T> eventType, IIntegrationEventHandler<T> handler);
+
 }
