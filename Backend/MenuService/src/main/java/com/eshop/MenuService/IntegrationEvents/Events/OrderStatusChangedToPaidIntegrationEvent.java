a03ccf111da9aa4ef38376072179d91d25850473
MinhTrinh04 <minhfananime@gmail.com>
2025-10-15 09:18:05 +0700

    Done Pub/Sub StockValidation

--- Backend/MenuService/src/main/java/com/eshop/MenuService/IntegrationEvents/Events/OrderStatusChangedToPaidIntegrationEvent.java
+++ Backend/MenuService/src/main/java/com/eshop/MenuService/IntegrationEvents/Events/OrderStatusChangedToPaidIntegrationEvent.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
 public class OrderStatusChangedToPaidIntegrationEvent extends IntegrationEvent {
-    private int orderId;
+    private UUID orderId;
     private List<OrderStockItem> orderStockItems;
 }
