5c12c206e1bf5f28f1c7d2e6c7d7a635ed853324
MinhTrinh04 <minhfananime@gmail.com>
2025-10-17 11:11:06 +0700

    Update Config 4 Rabbit

---
+++ Backend/PaymentService/src/main/java/com/eshop/PaymentService/Constants/PaymentConstants.java
@@ -0,0 +1,10 @@
+package com.eshop.PaymentService.Constants;
+
+public class PaymentConstants {
+    private PaymentConstants() {
+
+    }
+    public static final String QUEUE_NAME = "payment-service-queue";
+    public static final String  STATUS_200 = "200";
+    public static final String  MESSAGE_200 = "Request processed successfully";
+}
--- Backend/PaymentService/src/main/java/com/eshop/PaymentService/PaymentServiceApplication.java
+++ Backend/PaymentService/src/main/java/com/eshop/PaymentService/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.eshop.PaymentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.eshop.PaymentService", "com.eshop.buildingblocks"})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
