00c97237851315d444ea9127a1b7c270851e2c1a
MinhTrinh04 <minhfananime@gmail.com>
2025-11-26 22:30:55 +0700

    MenuService refined

--- Backend/MenuService/src/main/java/com/eshop/MenuService/Constants/MenuConstants.java
+++ Backend/MenuService/src/main/java/com/eshop/MenuService/Constants/MenuConstants.java
@@ -9,7 +9,7 @@ public class MenuConstants {
     public static final String ORDER_STATUS_CHANGE_TO_PAID_INTEGRATION_EVENT_V2 = "OrderStatusChangedToPaidIntegrationEventV2";
     public static final String ORDER_STATUS_CHANGE_TO_CANCELLED_INTEGRATION_EVENT = "OrderStatusChangedToCancelledIntegrationEvent";
     public static final String  STATUS_201 = "201";
-    public static final String  MESSAGE_201 = "Account created successfully";
+    public static final String  MESSAGE_201 = "Menu Item created successfully";
     public static final String  STATUS_200 = "200";
     public static final String  MESSAGE_200 = "Request processed successfully";
     public static final String  STATUS_417 = "417";
