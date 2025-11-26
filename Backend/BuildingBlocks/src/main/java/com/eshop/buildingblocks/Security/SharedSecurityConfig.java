3c096b887dddb5b1497d808db0d4c093ed440db5
MinhTrinh04 <minhfananime@gmail.com>
2025-11-26 10:39:37 +0700

    Migrate Security Config to building block

---
+++ Backend/BuildingBlocks/src/main/java/com/eshop/buildingblocks/Security/SharedSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.eshop.buildingblocks.Security;
+
+import com.eshop.buildingblocks.Security.Exception.CustomAccessDeniedHandler;
+import com.eshop.buildingblocks.Security.Exception.CustomAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+public class SharedSecurityConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return converter;
+    }
+
+    @Bean
+    public CustomAuthenticationEntryPoint customAuthenticationEntryPoint() {
+        return new CustomAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public CustomAccessDeniedHandler customAccessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+}
