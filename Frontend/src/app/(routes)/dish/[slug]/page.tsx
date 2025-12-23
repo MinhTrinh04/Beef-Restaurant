e46cfd83777912c2d931a7f76fd52176cdd32bc5
MinhTrinh04 <minhfananime@gmail.com>
2025-12-23 22:24:04 +0700

    usd -> vnd

--- Frontend/src/app/(routes)/dish/[slug]/page.tsx
+++ Frontend/src/app/(routes)/dish/[slug]/page.tsx
@@ -55,7 +55,7 @@ export default async function DishDetailPage({ params }: { params: { slug: strin
                 <div className="col-span-12 md:col-span-7">
                     <h1 className="dish__title !text-4xl mb-2">{serializedItem.name}</h1>
                     <div className="dish__content !border-b-0 mb-3 pb-0">
-                        <span className="dish__price">${serializedItem.price}</span>
+                        <span className="dish__price">{Math.round(serializedItem.price)}₫</span>
                     </div>
                     <div className="dish__description mb-6">
                         <span>{serializedItem.description}</span>
